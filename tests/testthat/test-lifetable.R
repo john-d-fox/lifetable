5b191ba6fa019a57a0326622e4e8f5be0660e203
John Fox <jfox@mcmaster.ca>
2025-01-24 15:49:17 -0500

    add a test

--- tests/testthat/test-lifetable.R
+++ tests/testthat/test-lifetable.R
@@ -1,5 +1,5 @@
-test_that("survival is computed correctly", {
-  lifetable <- as.vector(lifetable(rep(0.5, 10))[, "l[x]"])
+test_that("survival is computed correctly for single-year LT", {
+  lifetable <- as.vector(lifetable(rep(0.5, 10), a0=0.5, a1=0.5)[, "l[x]"])
   direct <- numeric(10)
   direct[1] <- 100000
   for (i in 2:10){
@@ -7,3 +7,18 @@ test_that("survival is computed correctly", {
   }
   expect_lte(max(abs(lifetable - direct)), 1)
 })
+
+test_that("survival is computed correctly for grouped-year LT", {
+  lifetable <- as.vector(lifetable(rep(0.1, 20), start=c(0, 1, seq(5, 90, by=5)), 
+                                   end=c(0, seq(4, 89, by=5), Inf), 
+                                   a0=0.5, a1=0.5)[, "l[x]"])
+  direct <- numeric(20)
+  direct[1] <- 100000
+  direct[2] <- round(direct[1]*0.9)
+  direct[3] <- round(direct[2]*(0.9^4))
+  for (i in 4:20){
+    direct[i] <- round(direct[i - 1]*(0.9^5))
+  }
+  expect_equal(lifetable, direct)
+})
+
