0f7819e5b449e695bbd0fb07f0b8751406442f51
John Fox <jfox@mcmaster.ca>
2025-02-02 15:54:08 -0500

    finish draft of vignette

--- vignettes/lifetable.R
+++ vignettes/lifetable.R
@@ -10,17 +10,18 @@ knitr::opts_chunk$set(
   dev = "png",
   comment = "#>" #,
 )
-.opts <- options(scipen=5)
+.opts <- options(scipen=10)
 
 ## ----CanadaDeaths2023---------------------------------------------------------
 library("lifetable")
 CanadaDeaths2023
 
 ## ----female-mortality---------------------------------------------------------
-with(CanadaDeaths2023, mortalityRates(population = population.female,
-                                      deaths = deaths.female,
-                                      start = start, end = end,
-                                      day = "2023-07-01"))
+(CanMort23 <- with(CanadaDeaths2023, 
+                   mortalityRates(population = population.female,
+                                  deaths = deaths.female,
+                                  start = start, end = end,
+                                  day = "2023-07-01")))
 
 ## ----mortality-data-----------------------------------------------------------
 CanadaMortality # 5-year age groups after age 4
@@ -34,9 +35,6 @@ args(lifetable)
 ## ----single-year-life-table---------------------------------------------------
 (lt.female <- lifetable(q = CanadaMortalitySingleYear$q.female))
 
-## ----coda, include=FALSE------------------------------------------------------
-options(.opts)
-
 ## ----female-male-survival-----------------------------------------------------
 lt.male <- lifetable(q = CanadaMortalitySingleYear$q.male)
 plot(0:109, lt.male[, "l[x]"]/1000, type="l", col="magenta", 
@@ -46,4 +44,21 @@ plot(0:109, lt.male[, "l[x]"]/1000, type="l", col="magenta",
  legend("bottomleft", inset=0.02, legend=c("female", "male"),
         lty=1:2, lwd=2, col=c("blue", "magenta"))
  grid()
+
+## ----recall-CanMort23---------------------------------------------------------
+CanMort23
+
+## ----life-table-5yr-----------------------------------------------------------
+with(CanMort23,
+     lifetable(q=mortality.rate, start=start, end=end))
+
+## ----CanadaMortality----------------------------------------------------------
+CanadaMortality
+
+## ----life-table-irregular-groups----------------------------------------------
+with(CanadaMortality,
+     lifetable(q=q.male, start=start, end=end))
+
+## ----coda, include=FALSE------------------------------------------------------
+options(.opts)
 
