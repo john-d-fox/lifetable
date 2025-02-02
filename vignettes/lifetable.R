993a59125e8436ab65d5d14806ca374256c95d44
John Fox <jfox@mcmaster.ca>
2025-02-01 19:30:19 -0500

    add vignette

---
+++ vignettes/lifetable.R
@@ -0,0 +1,49 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  message = TRUE,
+  warning = TRUE,
+  fig.align = "center",
+  fig.height = 6,
+  fig.width = 7,
+  fig.path = "fig/",
+  dev = "png",
+  comment = "#>" #,
+)
+.opts <- options(scipen=5)
+
+## ----CanadaDeaths2023---------------------------------------------------------
+library("lifetable")
+CanadaDeaths2023
+
+## ----female-mortality---------------------------------------------------------
+with(CanadaDeaths2023, mortalityRates(population = population.female,
+                                      deaths = deaths.female,
+                                      start = start, end = end,
+                                      day = "2023-07-01"))
+
+## ----mortality-data-----------------------------------------------------------
+CanadaMortality # 5-year age groups after age 4
+
+head(CanadaMortalitySingleYear) # single-year data
+tail(CanadaMortalitySingleYear) # single-year data
+
+## ----lifetable-args-----------------------------------------------------------
+args(lifetable)
+
+## ----single-year-life-table---------------------------------------------------
+(lt.female <- lifetable(q = CanadaMortalitySingleYear$q.female))
+
+## ----coda, include=FALSE------------------------------------------------------
+options(.opts)
+
+## ----female-male-survival-----------------------------------------------------
+lt.male <- lifetable(q = CanadaMortalitySingleYear$q.male)
+plot(0:109, lt.male[, "l[x]"]/1000, type="l", col="magenta", 
+      lwd=2, lty=2, xlab="Age", ylab="Percent Surviving")
+ lines(0:109, lt.female[, "l[x]"]/1000, type="l", col="blue",
+       lty=1, lwd=2)
+ legend("bottomleft", inset=0.02, legend=c("female", "male"),
+        lty=1:2, lwd=2, col=c("blue", "magenta"))
+ grid()
+
